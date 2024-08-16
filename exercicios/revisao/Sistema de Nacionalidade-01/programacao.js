df833911c7e0845748bf7f7dce7765577508163c
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2024-08-16 17:53:00 -0300

    Sistema de Nacionalidade (Concluído)

--- exercicios/revisao/Sistema de Nacionalidade-01/programacao.js
+++ exercicios/revisao/Sistema de Nacionalidade-01/programacao.js
@@ -82,7 +82,73 @@ function verificar() {
       res.appendChild(img)
       res.innerHTML += '<p>Você é <span style="font-weight: bold; color: #009440;">brasileiro</span></p>'
     } else if (nacionalidade.value == 'Paraguai') {
-
+      fundo.style.backgroundColor = '#C90F25'
+      titulo.style.color = 'white'
+      rodape.style.color = 'white'
+      img.setAttribute('src','paraguai.png')
+      res.appendChild(img)
+      res.innerHTML += '<p>eres <span style="font-weight: bold; color: #C90F25;">paraguayo</span></p>'
+    } else if (nacionalidade.value == 'Inglaterra') {
+      fundo.style.backgroundColor = 'white'
+      titulo.style.color = '#CD1928'
+      rodape.style.color = '#CD1928'
+      img.setAttribute('src', 'inglaterra.png')
+      res.appendChild(img)
+      res.innerHTML += '<p>You are <span style="font-weight: bold; color: #CD1928;">English</span></p>'
+    } else if (nacionalidade.value == 'Espanha') {
+      fundo.style.backgroundColor = '#C11F26'
+      titulo.style.color = '#FFC40E'
+      rodape.style.color = '#FFC40E'
+      img.setAttribute('src','espanha.png')
+      res.appendChild(img)
+      res.innerHTML += '<p>tu eres <span style="font-weight: bold; color: #C11F26;">español</span></p>'
+    } else if (nacionalidade.value == 'Alemanha') {
+      fundo.style.backgroundColor = 'black'
+      titulo.style.color = '#D60000'
+      rodape.style.color = '#F7C800'
+      img.setAttribute('src','alemanha.png')
+      res.appendChild(img)
+      res.innerHTML += '<p>Du bist <span style="font-weight: bold;">Deutscher</span></p>'
+    } else if (nacionalidade.value == 'Angola') {
+      fundo.style.backgroundColor = '#CC092F'
+      titulo.style.color = 'black'
+      rodape.style.color ='black'
+      img.setAttribute('src','angola.png')
+      res.appendChild(img)
+      res.innerHTML += '<p>Você é <span style="font-weight: bold; color: #F7C500;">angolano</span></p>'
+    } else if (nacionalidade.value == 'Egito') {
+      fundo.style.backgroundColor = 'white'
+      img.setAttribute('src', 'egito.png')
+      res.appendChild(img)
+      res.innerHTML += '<p>أنت <span style="font-weight: bold; color: #C91328;">مصري</span></p>'
+    } else if (nacionalidade.value == 'Madagascar') {
+      fundo.style.backgroundColor = '#F14038'
+      titulo.style.color = 'white'
+      rodape.style.color = 'white'
+      img.setAttribute('src','madagascar.png')
+      res.appendChild(img)
+      res.innerHTML += '<p><span style="font-weight: bold; color:#00803B; ">Madascarene</span> ianao</p>'
+    } else if (nacionalidade.value == 'Russia') {
+      fundo.style.backgroundColor = '#0036A8'
+      titulo.style.color = 'white'
+      rodape.style.color = 'white'
+      img.setAttribute('src','russia.png')
+      res.appendChild(img)
+      res.innerHTML += '<p>Ты <span style="font-weight: bold; color: #CE2A1D;">Русский</span></p>'
+    } else if (nacionalidade.value == 'China') {
+      fundo.style.backgroundColor = '#EF1620'
+      titulo.style.color = '#FFFF00'
+      rodape.style.color = '#FFFF00'
+      img.setAttribute('src','china.png')
+      res.appendChild(img)
+      res.innerHTML += '<p>你是 <span style="font-weight: bold; color: #EF1620;">中國人</span></p>'
+    } else if (nacionalidade.value == 'Índia') {
+      fundo.style.backgroundColor = '#F7651F'
+      titulo.style.color = 'white'
+      rodape.style.color = 'white'
+      img.setAttribute('src','india.png')
+      res.appendChild(img)
+      res.innerHTML += '<p>तुम हो <span style="font-weight: bold; color: #046736;">भारतीय</span></p>'
     }
   }
 }
